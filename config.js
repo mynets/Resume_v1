1d2fe8d897f481717b501f0e6c9507d6967af5e6
Diquan <diquanmoore@gmail.com>
2021-01-03 00:46:34 -0500

    second

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteTitle: "Diquan's Resume", // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/resume-icon.png',
+  pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Diquan',
+  lastName: 'Moore',
+  
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/mynets/Resume_v1',
+    },
+  ],
+  email: 'Diquanmoore@gmail.com',
+  //phone: '000-00000',
+  address: 'Miramar, FL'
+};
